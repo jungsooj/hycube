039e2cbe15b1b11ea8920e8b76417ff59b71fa2a
y.zamoum <Yesma-himane.ZAMOUM@isae-supaero.fr>
2022-06-21 11:03:37 +0200

    storing python script  outputs on a json file

--- python/hycube_optim/optim.py
+++ python/hycube_optim/optim.py
@@ -1,7 +1,9 @@
 from mip import Model, xsum, BINARY, maximize
+import json
 
+#electrical power for charging
 w=4.5
-t=10
+
 # times
 p=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
@@ -18,15 +20,23 @@ bat_min = 226.8
 p_max = 16
 
 Jobs=[]
-
-
-#time set for each job
-t=[5,2,3,10,3,5,3,30,3,3,25,7,2,2,2,2]
-
-# Electrical power 
-w1=[16.0,4.5,5.75,4.1,4.5,4.1,5.75,4.1,5.75,5.75,16,4.1,4.5,4.5,4.5,4.5]
 b=[]
 
+#number of jobs
+n=16
+
+#time set for each job
+t=[0,5,2,3,10,3,5,3,30,3,3,25,7,2,2,2,2,0]
+
+# Electrical power 
+w1=[0.0,16.0,4.5,5.75,4.1,4.5,4.1,5.75,4.1,5.75,5.75,16,4.1,4.5,4.5,4.5,4.5,0.0]
+
+S=[[1,2],[2,3],[3,4],[4,13],[5,6],[6,14],[7,8],[8,15],[9,10],[10,16],
+   [13,5],[14,7],[15,9],[16,11]]
+
+J= range(len(t))
+T=range(sum(t))
+time=0
 
 def minCheck(bat):
     if bat <= bat_min:
@@ -59,40 +69,63 @@ for i in range(len(t)):
     x= t[i] * w1[i]
     b.append(x)
 
-S=[[1,2],[2,3],[3,4],[4,13],[5,6],[6,14],[7,8],[8,15],[9,10],[10,16],
-   [13,5],[14,7],[15,9],[16,11]]
-
-J= range(len(p))
-  
 for j in J:
-    print(j)
-    print(bat_curr)
+    print(minCheck(bat_curr))
     if(j == 2 or j == 4 or j == 6 or j == 8):
         if(takePic(bat_curr) == True):
             bat_curr+= 4.59 * maxCharging(16,p,w,t[j])
             Jobs.append(j)
         else:
             bat_curr -= 4.1
-            print("im in the first else count")
     else:
-        print("im in the second else")
         Jobs.append(j)
         bat_curr -= b[j]
-    
-#print("________________ Jobs _______________________________")
-#for k in Jobs:
-    #print(Jobs[k])
-#print("______________________________________________________")
-#for i in range(len(t)):
-    #print("job {} starts at {} and finish at {}".format(Jobs[i],t[i+1]-t[i],t[i]+t[i-1]))
-    #print(i)
-    
-    
 
-    
-    
+dicts={}
+print("_____________________ Jobs ___________________________")
+print("______________________________________________________")
+for j in J:
+    print("job {} starts at {} and finish at {}".format(Jobs[j],time,time+t[j]))
+    time+= t[j]
+    dicts[Jobs[j]] = [time, time+t[j]]
+print("______________________________________________________")
 
- 
+with open("dicts.json", 'w') as output:
+    json.dump(dicts,output)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
